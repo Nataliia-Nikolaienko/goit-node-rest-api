eee66dfe53facf45f4a7c9373d315d5f3585f030
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2024-01-22 21:14:23 +0100

    update contactsSchemas, validateBody

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -3,6 +3,15 @@ import HttpError from "../helpers/HttpError.js";
 const validateBody = (schema) => {
   const func = (req, _, next) => {
     const { error } = schema.validate(req.body);
+    const contactLength = Object.keys(req.body).length;
+    if (contactLength < 1) {
+      next(
+        HttpError(
+          400,
+          "Please enter at least one field (name, email or phone) to update the contact"
+        )
+      );
+    }
     if (error) {
       next(HttpError(400, error.message));
     }
